7f054fd3653ea5caa19cec43b1fb82e58698f630
yaameenc <yaameenchoudhury@gmail.com>
2022-05-22 13:29:23 +0800

    Added hamburger menu and the main play section using how to play

---
+++ questions.js
@@ -0,0 +1,84 @@
+export const questions = [
+{
+    q: "What is C in CSS?",
+    a: "Cascading",
+},
+{
+    q: "The browser applies the feature of a tag until it encounters_________tag?",
+    a: "Closing",
+},
+{
+    q: "What is the general syntax for inline image?",
+    a: "img src=file",
+  },
+  {
+    q: "Tag for inline frame is _________",
+    a: "Iframe",
+  },
+  {
+    q: "JavaScript is an ___________ language",
+    a: "Object-Oriented",
+  },
+  {
+    q: "How can a datatype be declared to be a constant type?",
+    a: "const",
+  },
+  {
+    q: "What is the output of the following code snippet - print(NaN === NaN)?",
+    a: "false",
+  },
+
+  {
+    q: "Which function is used to serialize an object into a JSON string in Javascript?",
+    a: "stringify()",
+  },
+
+  {
+    q: "What keyword is used to declare an asynchronous function in Javascript?",
+    a: "async",
+  },
+
+  {
+    q: "How to stop an interval timer in Javascript?",
+    a: "clearInterval",
+  },
+
+  {
+    q: "________ returns the character in the string starting at the specified position via the specified number of characters?",
+    a: "substr()",
+  },
+
+  {
+    q: "In a case, where the value of the operator is NULL , the typeof returned by the unary operator is___",
+    a: "object",
+  },
+
+  {
+    q: "A collection of elements of the same data type which may either in order or not, is called _____.",
+    a: "Array",
+  },
+
+  {
+    q: ` In the following line of code, what we will call the "datatype" written in brackets -
+    article[datatype]=assignment_value`,
+    a: "String",
+  },
+
+  {
+    q: "If a function which does not return a value is known as _____.",
+    a: "Procedures",
+  },
+
+  {
+    q: " ______ class in bootstrap is used to create a big box for calling extra attention.",
+    a: "jumbotron",
+  },
+
+  {
+    q: " ______ class is used to create a black navigation bar.",
+    a: "navbar-inverse",
+  },
+
+
+
+];
